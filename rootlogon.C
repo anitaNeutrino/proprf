cb88c2a019037d8198c9357b2baa9e66a302027d
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2016-09-23 08:50:40 -0500

    add reasonable rootlogon

---
+++ rootlogon.C
@@ -0,0 +1,9 @@
+{
+
+  gSystem->Load("${ANITA_UTIL_INSTALL_DIR}/lib/libRootFftwWrapper.so"); 
+  gSystem->Load("${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaEvent.so"); 
+  gSystem->Load("lib/libproprf.so"); 
+
+
+}
+
